ae4dd12867df2434ba0be2b604c18c532467f5ce
Jean Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2019-02-01 13:56:26 -0500

    Update todos.blade.php

--- resources/views/perfil/pedidos/todos.blade.php
+++ resources/views/perfil/pedidos/todos.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-12">
             <h1> <i class="fa fa-truck"></i> Pedidos</h1>
-            <h4>Tienes <b>0</b> pedidos en total</h4>
+            <h4>Tienes <b>{{$pedidos->total()}}</b> pedidos en total</h4>
         </div>
     </div>
 
@@ -15,16 +15,34 @@
 
         <div class="col-md-8 py-2 py-md-0">
                 <div class="list-group">
-                        @for ($i = 0; $i < 5; $i++)
-                                <a href="#" class="list-group-item list-group-item-action mb-2 hoverable">
-                                  <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Nombre del Producto</h5>
-                                    <small>3 days ago</small>
-                                  </div>
-                                  <h6 class="mb-1 font-weight-bold">$300.000,00</h6>
-                                  <small>Estado</small>
-                                </a>
-                                @endfor
+                    @forelse ($pedidos as $pedido)
+                    <a href="#" class="list-group-item list-group-item-action mb-2 hoverable">
+                            <span class="badge badge-secondary">{{$pedido->estado->nombre}}</span>
+                      <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">{{$pedido->producto->nombre}}</h5>
+                        <small>
+                                {{$pedido->created_at->diffForHumans()}}
+                        </small>
+                      </div>
+                      <h6 class="mb-1 font-weight-bold"><span>$ </span>{{$pedido->producto->precio}}</h6>
+                      <small>
+                           Ultima actualizacion: <b>{{$pedido->updated_at->diffForHumans()}}
+                            </b>
+                        </small>
+                    </a>
+                    @empty
+                        <div class="text-center">
+                                <h4>No Has realizados pedidos</h4>
+                                <p>Anímate a realizar uno</p>
+                        </div>
+                    @endforelse
+                            </div>
+                            <div class="my-3">
+                                    <nav aria-label="Page navigation example">
+                                            <ul class="pagination justify-content-center">
+                                              {{ $pedidos->links() }}
+                                            </ul>
+                                          </nav>
                             </div>
         </div>
 
