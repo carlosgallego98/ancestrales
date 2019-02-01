d6527487b5cded5417f4b7be0c10bc8893574c03
Jean Carlos <gallegomosquerajeancarlos@gmail.com>
2019-02-01 13:25:26 -0500

    detalles pedido

--- resources/views/perfil/detalles_pedido.blade.php
+++ resources/views/perfil/detalles_pedido.blade.php
@@ -4,31 +4,37 @@
 @section('contenido')
 <div class="container">
     <div class="row">
-
-
-
-<h6 class="text-muted p-0 m-0">Pedido:</h6>
-                <h4 class="p-0 m-0"><i class="fa fa-cocktail mr-1"></i><b>{{$pedido->producto->nombre}}</b></h5>
-                <p class="p-0 m-0"><i class="far fa-clock mr-1"></i>   Realizado el {{$pedido->created_at}}</p>
-
+        <div class="col-12 py-3">
+            <h4 class="display-4">
+            <i class="fa fa-cart-arrow-down"></i>
+            <b>Detalles del Pedido</b>
+            </h4>
+            <h6><p class="p-0 m-0 text-muted"><i class="far fa-clock mr-1"></i>Realizado el <b>{{$pedido->created_at}}</b></p></h6>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-8">
-            <div class="card card-body mb-3">
-                <div class="row">
-                    <div class="col-md-4">
-                    </div>
+            <div class="card card-body mb-3" style="min-height:220px;">
+                <h6 class="text-muted p-0 m-0">Detalle del Producto</h6>
+
+                <div class="row py-2">
+                    <div class="col-md-4 flex-center">...</div>
                     <div class="col-md-8">
-                        <h5>Nombre del Producto</h5>
-                        <h6>Precio</h6>
-                        <p>Descripcion recortada</p>
+                        <h4><b>{{$pedido->producto->nombre}}</b></h4>
+                        <h6 class="font-weight-bold"><span>$</span>{{$pedido->producto->precio}}</h6>
+                        <div style="  white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">
+                            <p>{{$pedido->producto->descripcion}}</p>
+                        </div>
                         <div class="text-right">
-                            <a href="#">Detalles</a>
+                            <a href="#">Ver Más</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card card-body text-center mb-3">
+
+        <div class="col-md-4"">
+            <div class="card card-body text-center mb-3" style="min-height:220px;">
                 <i class="fa fa-cart-arrow-down fa-3x m-2"></i>
                     <small class="text-muted">Estado del Pedido</small>
                     <h5><b>{{$pedido->estado->nombre}}</b></h5>
@@ -38,6 +44,7 @@
                 </p>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
